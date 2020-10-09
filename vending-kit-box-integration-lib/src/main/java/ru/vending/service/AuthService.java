8944561443575069b46f5ff7ffb2c68e5494c843
Alexey Vasin <aevasin@vtb.ru>
2020-10-09 19:18:59 +0300

    remove unused mapper

--- vending-kit-box-integration-lib/src/main/java/ru/vending/service/AuthService.java
+++ vending-kit-box-integration-lib/src/main/java/ru/vending/service/AuthService.java
@@ -1,29 +1,22 @@
 package ru.vending.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 import ru.vending.dto.AuthDto;
 import ru.vending.dto.AuthDtoWrapper;
 import ru.vending.dto.AuthDtoWrapperWithId;
-import ru.vending.mapper.AuthWrapperMapper;
 
 import java.util.Date;
 
 @Service
 @Slf4j
 public class AuthService {
+
+    //TODO @Value
     private static final String PASSWORD = "11qq22ww33ee";
     private static final String USER_LOGIN = "Api_User";
     private static final Integer COMPANY_ID = 383361;
-
-    private final AuthWrapperMapper authWrapperMapper;
-
-    @Autowired
-    public AuthService (AuthWrapperMapper authWrapperMapper) {
-        this.authWrapperMapper = authWrapperMapper;
-    }
 
     private Long generateUniqueRequestId () {
         Date date = new Date();
