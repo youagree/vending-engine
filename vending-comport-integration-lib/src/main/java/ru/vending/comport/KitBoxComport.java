5cc7051872bf8e20d6bc08b26031f3b402d742a1
PavelSoldatov <fastwizardr@gmail.com>
2020-10-13 16:06:23 +0300

    Fix Lombok и перенес описание

--- vending-comport-integration-lib/src/main/java/ru/vending/comport/KitBoxComport.java
+++ vending-comport-integration-lib/src/main/java/ru/vending/comport/KitBoxComport.java
@@ -1,25 +1,19 @@
 package ru.vending.comport;
 
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
 /**
  * Класс реализующий возможность отправить данные на компорт KitBox (String) и вычитывать ответ (String).
  * Настройки порта: 9600.8.1.NONE
  */
 
-import jssc.SerialPort;
-import jssc.SerialPortException;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.SneakyThrows;
-import org.springframework.stereotype.Component;
-
 @Component
 public class KitBoxComport {
     private static final SerialPort kitBoxComport = new SerialPort("COM7");
     // TODO узнать адрес порта, который будет соединяться с KitBox
-
-    @Getter
-    @Setter
-    private String result;
 
     @SneakyThrows(value = {SerialPortException.class, InterruptedException.class})
     public void sendDataOnKitBoxComport(String data) {
@@ -28,7 +22,6 @@ public class KitBoxComport {
         kitBoxComport.writeBytes(data.getBytes());
         Thread.sleep(100);
         kitBoxComport.closePort();
-
     }
 
     @SneakyThrows(value = {SerialPortException.class, InterruptedException.class})
--- vending-comport-integration-lib/src/main/java/ru/vending/comport/SpiralMotorComport.java
+++ vending-comport-integration-lib/src/main/java/ru/vending/comport/SpiralMotorComport.java
@@ -1,25 +1,18 @@
 package ru.vending.comport;
 
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import java.util.Arrays;
+
 /**
  * Класс реализующий деятельность компорта связанного с моторами спиралей, умеет писать и
  * ждать ответа о завершении.
  * Настройки порта: 9600.8.1.NONE
  */
 
-import jssc.SerialPort;
-import jssc.SerialPortException;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-
-@Data
 @Component
-@AllArgsConstructor
-@NoArgsConstructor
 public class SpiralMotorComport {
     private static final SerialPort raspberryComport = new SerialPort("COM5"); // TODO узнать адрес этого порта на расбери
 
@@ -30,7 +23,6 @@ public class SpiralMotorComport {
         raspberryComport.writeInt(spiralNumber);
         Thread.sleep(100);
         raspberryComport.closePort();
-
     }
 
     @SneakyThrows(value = {SerialPortException.class, InterruptedException.class})
