59e77e0554c074c622545aace40919e8d30b02c4
PavelSoldatov <fastwizardr@gmail.com>
2020-10-28 21:31:15 +0300

    Добавил контроллер и сервис для запуска моторов

---
+++ vending-engine-impl/src/main/java/ru/vending/service/MotorsService.java
@@ -0,0 +1,42 @@
+package ru.vending.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.vending.api.ComportInterfaceIntegration;
+import ru.vending.entity.Product;
+import ru.vending.repository.ProductRepository;
+
+import javax.persistence.EntityNotFoundException;
+
+@Slf4j
+@Service
+public class MotorsService {
+
+    private ComportInterfaceIntegration comportInterfaceIntegration;
+    private ProductRepository productRepository;
+
+    @Autowired
+    public MotorsService(ComportInterfaceIntegration comportInterfaceIntegration, ProductRepository productRepository) {
+        this.comportInterfaceIntegration = comportInterfaceIntegration;
+        this.productRepository = productRepository;
+    }
+
+    public String activateMotorsByChoiceNumber(Integer choiceNumber, Long id) {
+        comportInterfaceIntegration.spiralMotorInput(String.valueOf(choiceNumber));
+        if (comportInterfaceIntegration.spiralMotorWaiting().equals("1")) {
+            Product product = productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            if (product.getCount() >= 1) {
+                product.setCount(product.getCount() - 1);
+                productRepository.save(product);
+            }
+
+            return "Покупка выполнена!";
+        } else {
+            log.info("Error! Try again please!");
+            return "Произошла ошибка!";
+        }
+
+    }
+}
+
