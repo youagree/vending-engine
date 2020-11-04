cb93c5e7193cb0f09b1eba06ead82c406c3986c1
PavelSoldatov <fastwizardr@gmail.com>
2020-11-04 15:16:54 +0300

    Response status

--- vending-engine-impl/src/main/java/ru/vending/controller/ProductsController.java
+++ vending-engine-impl/src/main/java/ru/vending/controller/ProductsController.java
@@ -1,11 +1,9 @@
 package ru.vending.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.vending.dto.ProductDto;
 import ru.vending.service.ProductService;
 
@@ -23,11 +21,13 @@ public class ProductsController {
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<ProductDto> getProductById(@PathVariable Long id) {
         return ResponseEntity.ok(productService.getProductById(id));
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<ProductDto>> getAllProducts() {
         return ResponseEntity.ok(productService.getAllProducts());
     }
